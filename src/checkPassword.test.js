275e0fa0be61d7f7a5a9dc12dea518803839306b
OleksiiMishchenko <alex.mishch@gmail.com>
2024-11-24 20:50:37 +0200

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -3,17 +3,43 @@
 describe(`Function 'checkPassword':`, () => {
   const checkPassword = require('./checkPassword');
 
-  it(`should be declared`, () => {
-    expect(checkPassword).toBeInstanceOf(Function);
-  });
+  it(`should be declared`, () => expect(checkPassword).toBeDefined());
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('Password1!')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('Password1!')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' for the valid password with 16 characters`, () => {
+    expect(checkPassword('PasswordPasswd1!')).toBeTruthy();
+  });
+
+  it(`should return 'false' for the password with cyrillic characters`, () => {
+    expect(checkPassword('Пароль1!')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password with length < 8`, () => {
+    expect(checkPassword('Pass1!')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password with length > 16`, () => {
+    expect(checkPassword('PasswordPassword1!')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password without at least 1 digit`, () => {
+    expect(checkPassword('Password!')).toBeFalsy();
+  });
+
+  it(
+    `should return 'false' for password without at least 1 special symbol`,
+    () => expect(checkPassword('Password1')).toBeFalsy()
+  );
+
+  it(
+    `should return 'false' for password without at least 1 uppercase letter`,
+    () => expect(checkPassword('password1!')).toBeFalsy()
+  );
 });
